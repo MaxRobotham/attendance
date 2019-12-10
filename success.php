27d4f2690bcbbfddb69eb6b320dbcee399ca5931
Max Robotham <m_Robotham@hotmail.com>
2019-12-10 04:09:39 -0500

    using $_POST instead of $_GET
    used the post ($_POST) so that the information is not passed in the URL.
    this includes changing the form method = "post" (index.php) and changing echo $_GET to POST (Success.php)

--- success.php
+++ success.php
@@ -6,31 +6,71 @@
 
 <h1 class ="text-center text-success"> You Have Been Registered</h1>
 
-<div class="card" style="width: 18rem;">
+<!-- this prints out vales that were passed to the action page using method="get" -->
+<!-- <div class="card" style="width: 18rem;">
     <div class="card-body">
         <h5 class="card-title">
-        <?php echo $_GET['firstname'].' '.$_GET['lastname']; ?>
+        <?php //echo $_GET['firstname'].' '.$_GET['lastname']; ?>
         </h5>
 
         <h6 class="card-subtitle mb-2 text-muted">
-        <?php echo $_GET['specialty']; ?>
+        <?php //echo $_GET['specialty']; ?>
         </h6>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="card-link">Card link</a>
-        <a href="#" class="card-link">Another link</a>
+        <p class="card-text">
+        Date of Birth: <?php //echo $_GET['dob']; ?>
+        </p>
+
+        <p class="card-text">
+        Email: <?php //echo $_GET['email']; ?>
+        </p>
+
+        <p class="card-text">
+        Phone Number: <?php //echo $_GET['phone']; ?>
+        </p>
+
+        <a href="../attendance/index.php" class="card-link">Back to registration </a>
+       
+    </div>
+
+</div> -->
+
+
+<div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <h5 class="card-title">
+        <?php echo $_POST['firstname'].' '.$_POST['lastname']; ?>
+        </h5>
+
+        <h6 class="card-subtitle mb-2 text-muted">
+        <?php echo $_POST['specialty']; ?>
+        </h6>
+        <p class="card-text">
+        Date of Birth: <?php echo $_POST['dob']; ?>
+        </p>
+
+        <p class="card-text">
+        Email: <?php echo $_POST['email']; ?>
+        </p>
+
+        <p class="card-text">
+        Phone Number: <?php echo $_POST['phone']; ?>
+        </p>
+
+        <a href="../attendance/index.php" class="card-link">Back to registration </a>
+       
     </div>
 
 </div>
 
-    <?php
-        echo $_GET['firstname'];
-        echo $_GET['lastname'];
-        echo $_GET['dob'];
-        echo $_GET['specialty'];
-        echo $_GET['email'];
-        echo $_GET['phone'];
+    <!-- <?php//
+        //echo $_GET['firstname'];
+        //echo $_GET['lastname'];
+        //echo $_GET['dob'];
+        //echo $_GET['specialty'];
+        //echo $_GET['email'];
+        //echo $_GET['phone'];
 
-    ?>
+    ?> -->
 
 <br>
 <br>
